40715fad6220a6446e27f3e351d191bd25d0e769
Barry Loper <barry.loper@centurylink.com>
2017-10-26 11:06:10 -0600

    basic api working

--- db.py
+++ db.py
@@ -1,40 +1,44 @@
 import sqlite3
-import sys
-import os
 import csv
 import pkg_resources
 from pathlib import Path
+from functools import partial
+
+__all__ = ['initialize_db', 'get_artists', 'get_album', 'update_album', 'create_album', 'delete_album']
 
 DB_LOCATION = Path.home() / 'hifi.sqlite'
+
+connect = partial(sqlite3.connect, str(DB_LOCATION))
 
 
 def initialize_db():
     CSV_FILE = pkg_resources.resource_filename('hifi', 'data/albums.csv')
     tables = """
         CREATE TABLE 'artists' (
-          id INTEGER PRIMARY KEY,
+          artist_id INTEGER PRIMARY KEY,
           name TEXT UNIQUE NOT NULL
         );
         CREATE TABLE 'genres' (
-          id INTEGER PRIMARY KEY,
+          genre_id INTEGER PRIMARY KEY,
           name TEXT UNIQUE NOT NULL
         );
         CREATE TABLE 'albums' (
-          id INTEGER PRIMARY KEY,
-          name TEXT UNIQUE NOT NULL,
+          album_id INTEGER PRIMARY KEY,
+          name TEXT NOT NULL,
           year INTEGER NOT NULL,
-          artist INTEGER NOT NULL,
-          genre INTEGER NOT NULL,
-          FOREIGN KEY(artist) REFERENCES artists(id),
-          FOREIGN KEY(genre) REFERENCES genres(id)
+          artist_id INTEGER NOT NULL,
+          genre_id INTEGER NOT NULL,
+          UNIQUE (name, artist_id),
+          FOREIGN KEY(artist_id) REFERENCES artists(artist_id),
+          FOREIGN KEY(genre_id) REFERENCES genres(genre_id)
         );
     """
 
-    with sqlite3.connect(str(DB_LOCATION)) as conn:
+    with connect() as conn:
         curr = conn.cursor()
         curr.executescript(tables)
 
-    with sqlite3.connect(str(DB_LOCATION)) as conn:
+    with connect() as conn:
         curr = conn.cursor()
         with open(CSV_FILE, 'r',
                   encoding='cp1252') as f:  # csv has a non-standard encoding
@@ -46,7 +50,7 @@ def initialize_db():
                     artist_id = curr.lastrowid
                 except sqlite3.IntegrityError as e:
                     """ Artist already exists """
-                    curr.execute("""SELECT id from artists WHERE name=?""", (row['artist'],))
+                    curr.execute("""SELECT artist_id from artists WHERE name=?""", (row['artist'],))
                     artist = curr.fetchone()
                     artist_id = artist[0]
                 try:
@@ -54,8 +58,131 @@ def initialize_db():
                     genre_id = curr.lastrowid
                 except sqlite3.IntegrityError as e:
                     """ genre already exists """
-                    curr.execute("""SELECT id FROM genres WHERE name=?""", (row['genre'],))
+                    curr.execute("""SELECT genre_id FROM genres WHERE name=?""", (row['genre'],))
                     genre = curr.fetchone()
                     genre_id = genre[0]
 
-                curr.execute("""INSERT INTO 'albums' (name, artist, genre, year) VALUES (?, ?, ?, ?)""", (row['album'], artist_id, genre_id, int(row['year'])))
+                curr.execute("""INSERT INTO 'albums' (name, artist_id, genre_id, year) VALUES (?, ?, ?, ?)""", (row['album'], artist_id, genre_id, int(row['year'])))
+
+
+def get_artists():
+    with connect() as conn:
+        curr = conn.execute("""SELECT name FROM artists""")
+        results = curr.fetchall()
+    return [{'name': result[0]} for result in results]
+
+
+def _create_artist(name):
+    """ returns database id
+        does not create duplicates (case-sensitive)
+    """
+    with connect() as conn:
+        curr = conn.execute("""SELECT artist_id FROM artists WHERE name=?""", (name,))
+        existing_artist = curr.fetchone()
+        if existing_artist is None:
+            curr.execute("""INSERT INTO artists(name) VALUES (?)""", (name,))
+            artist_id = curr.lastrowid
+        else:
+            artist_id = existing_artist[0]
+
+    return artist_id
+
+
+def _create_genre(name):
+    """ returns database id
+        does not create duplicates (case-sensitive)
+    """
+    with connect() as conn:
+        curr = conn.execute("""SELECT genre_id FROM genres WHERE name=?""", (name,))
+        existing_genre = curr.fetchone()
+        if existing_genre is None:
+            curr.execute("""INSERT INTO genres(name) VALUES (?)""", (name,))
+            genre_id = curr.lastrowid
+        else:
+            genre_id = existing_genre[0]
+
+    return genre_id
+
+
+def create_album(name, artist, genre, year):
+    """ returns database id if insert is successful """
+    artist_id = _create_artist(artist)
+    genre_id = _create_genre(genre)
+    with connect() as conn:
+        curr = conn.execute("""INSERT INTO albums(name, artist_id, genre_id, year) 
+                               VALUES (?, ?, ?, ?)""", (name, int(artist_id), int(genre_id), int(year)))
+    return curr.lastrowid
+
+
+def _get_album_row(name, artist):
+    with connect() as conn:
+        curr = conn.execute(""" SELECT albums.name AS album, 
+                                       artists.name AS artist, 
+                                       genres.name AS genre, 
+                                       albums.year AS year,
+                                       album_id,
+                                       artist_id,
+                                       genre_id
+                                FROM albums 
+                                JOIN artists USING (artist_id)
+                                JOIN genres USING (genre_id)
+                                WHERE album=? AND artist=?
+                            """, (name, artist))
+        result = curr.fetchone()
+    return result
+
+def get_album(name, artist):
+    """ get a single album by unique name and artist """
+    result = _get_album_row(name, artist)
+
+    if result is not None:
+        return {'name': result[0],
+                'artist': result[1],
+                'genre': result[2],
+                'year': result[3]}
+    else:
+        return result
+
+
+def get_artist_albums(artist):
+    """ this will be useful for the "index of artists requirement"
+        in an index of artists, each name could link to a list of albums
+    """
+    raise NotImplementedError
+
+
+def get_genre_albums(genre):
+    raise NotImplementedError
+
+
+def get_year_albums(year):
+    raise NotImplementedError
+
+
+def update_album(old_name, old_artist, name, artist, genre, year):
+    """ should return 1 if successful """
+    current_album_row = _get_album_row(old_name, old_artist)
+    print(current_album_row)
+    with connect() as conn:
+        new_artist_id = _create_artist(artist)
+        new_genre_id = _create_genre(genre)
+        curr = conn.execute("""UPDATE albums 
+                               SET name=?,
+                                   artist_id=?,
+                                   genre_id=?,
+                                   year=?
+                               WHERE album_id=?
+                            """,
+                            (name, new_artist_id, new_genre_id, year, current_album_row[4])
+                            )
+
+    return curr.rowcount
+
+
+def delete_album(name, artist):
+    """ returns number of deleted rows"""
+    with connect() as conn:
+        curr = conn.execute("""DELETE FROM albums WHERE name=? AND 
+                                  artist_id=(SELECT artist_id FROM artists WHERE name=?)""",
+                            (name, artist))
+    return curr.rowcount
--- server.py
+++ server.py
@@ -1,12 +1,55 @@
 import hug
+import falcon
 import os
-from hifi.db import initialize_db, DB_LOCATION
+from hifi import db
+
+hug.API(__name__).http.output_format = hug.output_format.pretty_json
 
 
-def main():
-    if not os.path.exists(DB_LOCATION):
-        initialize_db()
+@hug.get('/artists')
+def artists():
+    return db.get_artists()
+
+@hug.get('/album/{name}/{artist}')
+@hug.local()
+def get_album(name, artist, response):
+    album = db.get_album(name=name, artist=artist)
+    if album is None:
+        response.status = falcon.HTTP_404
+    else:
+        return album
 
 
-if __name__ == '__main__':
-    main()
+@hug.post('/album')
+def create_album(name: hug.types.text,
+                 artist: hug.types.text,
+                 genre: hug.types.text,
+                 year: hug.types.number):
+    album_id = db.create_album(name, artist, genre, year)
+    return album_id
+
+
+@hug.put('/album/{name}/{artist}')
+def update_album(name: hug.types.text,
+                 artist: hug.types.text,
+                 new_name: hug.types.text,
+                 new_artist: hug.types.text,
+                 new_genre: hug.types.text,
+                 new_year: hug.types.text,
+                 response):
+    result = db.update_album(name, artist, new_name, new_artist, new_genre, new_year)
+
+    if result is not 1:
+        # update failed. probably because name and artist didn't match an album
+        response.status = falcon.HTTP_404
+
+
+@hug.delete('/album/{name}/{artist}')
+def delete_album(name: hug.types.text,
+                 artist: hug.types.text,
+                 response):
+    result = db.delete_album(name, artist)
+
+    if result is not 1:
+        # delete failed
+        response.status = falcon.HTTP_404
