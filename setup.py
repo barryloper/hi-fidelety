7e7ce12d6c1ba2b0ca2109c8dd04993002a878a2
Barry Loper <barry.loper@centurylink.com>
2017-11-04 00:36:25 -0600

    :bug: fixed csv pathname in setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 setup(
     name="High Fidelity",
-    version="0.1",
+    version="0.1.1",
     author='Barry Loper',
     license='MIT',
     packages=find_packages(),
     python_requires='==3.6.*',
     install_requires=['hug==2.3.2'],
-    package_data={'': 'albums.csv'},
+    package_data={'': 'data/albums.csv'},
     entry_points={
         'console_scripts': [
             'hifi-api = hifi.server:main'
